39198c9746534fbdfe5cee3b2506f54c4a83898f
unknown <wolwx@hotmail.com>
2025-10-22 23:02:22 +0200

    fix(android): nouvelle tentative de correction du bug de démarrage Android
    - Correction du nommage de l'application dans les fichiers web
    - Tentative de résolution du crash au démarrage sur Android
    - Mise à jour des métadonnées et manifest pour cohérence
    - Relance du workflow de build automatisé

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,33 +5,33 @@ plugins {
 }
 
 android {
-    namespace = "com.xr.xrtechtools"  // ← CHANGÉ
-    compileSdk = 34  // ← FIXÉ à 34
+    namespace = "com.xr.xrtechtools"
+    compileSdk = 34
     ndkVersion = "25.1.8937393"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8  // ← CHANGÉ de 11 à 8
-        targetCompatibility = JavaVersion.VERSION_1_8  // ← CHANGÉ de 11 à 8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"  // ← CHANGÉ de 11 à 8
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
-        applicationId = "com.xr.xrtechtools"  // ← CHANGÉ
-        minSdk = 21  // ← FIXÉ à 21 (au lieu de flutter.minSdkVersion)
-        targetSdk = 34  // ← FIXÉ à 34
+        applicationId = "com.xr.xrtechtools"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
         
-        multiDexEnabled = true  // ← AJOUTÉ
+        multiDexEnabled = true
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false  // ← AJOUTÉ (désactive ProGuard)
-            isShrinkResources = false  // ← AJOUTÉ
+            isMinifyEnabled = false
+            isShrinkResources = false
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -42,5 +42,5 @@ flutter {
 }
 
 dependencies {
-    implementation("androidx.multidex:multidex:2.0.1")  // ← AJOUTÉ
+    implementation("androidx.multidex:multidex:2.0.1")
 }
